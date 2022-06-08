3ad83b0ff2939ca21c4666740dcf0e0fdc117b77
Elvis <elvisdurmishi@gmail.com>
2022-06-08 16:26:10 +0200

    Made all the inputs functional

--- src/App.js
+++ src/App.js
@@ -6,17 +6,51 @@ import MainInput from "./components/MainInput/index";
 import axios from "axios";
 
 function App() {
-  const [type, setType] = useState("text");
+  const initialColors = {
+    background: "#00288a",
+    background_color: [0, 40, 138],
+    foreground: "#ffffff",
+    foreground_color: [255, 255, 255],
+  };
 
+  const [type, setType] = useState("text");
+  const [body, setBody] = useState("");
+  const [colors, setColors] = useState(initialColors);
+  const [label, setLabel] = useState("");
   const baseURL = "https://qr-code-generator-api.herokuapp.com/api";
 
   const generateQRCode = () => {
     axios
-      .post(baseURL + "/generate", {
-        body: "Test",
+      .post(
+        baseURL + "/generate",
+        {
+          body: body,
+          label: label,
+          background_color: colors.background_color,
+          foreground_color: colors.foreground_color,
+        },
+        { responseType: "blob" }
+      )
+      .then(function (response) {
+        handleResponse(response);
       })
-      .then(function (response) {})
       .catch(function (error) {});
+  };
+
+  const handleResponse = (response) => {
+    const url = window.URL.createObjectURL(new Blob([response.data]));
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", "qr-code.png");
+    document.body.appendChild(link);
+    link.click();
+  };
+
+  const resetInput = () => {
+    setType("text");
+    setBody("");
+    setColors(initialColors);
+    setLabel("");
   };
 
   return (
@@ -25,11 +59,18 @@ function App() {
         <div className="tab active" id="1">
           <div className="main-input-content">
             <Sidebar type={type} setType={setType} />
-            <MainInput />
+            <MainInput body={body} setBody={setBody} type={type} />
           </div>
         </div>
         <div className="tab active" id="2">
-          <SettingsCard generate={generateQRCode} />
+          <SettingsCard
+            generate={generateQRCode}
+            colors={colors}
+            setColors={setColors}
+            label={label}
+            setLabel={setLabel}
+            resetInput={resetInput}
+          />
         </div>
       </div>
     </div>
--- src/components/Settings/SettingsCard.js
+++ src/components/Settings/SettingsCard.js
@@ -3,7 +3,14 @@ import Button from "../Common/Button";
 import Accordion from "./Accordion";
 import { CgSoftwareDownload } from "react-icons/cg";
 
-const SettingsCard = ({ generate }) => {
+const SettingsCard = ({
+  generate,
+  colors,
+  setColors,
+  label,
+  setLabel,
+  resetInput,
+}) => {
   return (
     <div
       className={
@@ -12,16 +19,23 @@ const SettingsCard = ({ generate }) => {
     >
       <div className="qr-code-image" />
       <div className={"flex flex-col items-center w-full row-span-2"}>
-        <Accordion text={"Colors"} items={<Colors />} />
-        <Accordion text={"Label"} items={<LabelInput />} />
+        <Accordion
+          text={"Colors"}
+          items={<Colors colors={colors} setColors={setColors} />}
+        />
+        <Accordion
+          text={"Label"}
+          items={<LabelInput label={label} setLabel={setLabel} />}
+        />
       </div>
       <div className={"flex gap-4 my-5"}>
         <Button
           onClick={() => {
             generate();
+            resetInput();
           }}
           icon={<CgSoftwareDownload size={28} />}
-          text={"JPG"}
+          text={"PNG"}
           color={"lightblue"}
         />
       </div>
@@ -29,7 +43,20 @@ const SettingsCard = ({ generate }) => {
   );
 };
 
-const Colors = () => {
+const Colors = ({ colors, setColors }) => {
+  const handleColorChange = (type, color) => {
+    var hex_code = color.split("");
+    var red = parseInt(hex_code[1] + hex_code[2], 16);
+    var green = parseInt(hex_code[3] + hex_code[4], 16);
+    var blue = parseInt(hex_code[5] + hex_code[6], 16);
+    var rgb = [red, green, blue];
+    let newColors = Object.assign({}, colors);
+    newColors[type] = color;
+    newColors[type + "_color"] = rgb;
+
+    setColors(newColors);
+  };
+
   return (
     <div className={"flex flex-col gap-3"}>
       <div className={"flex flex-col items-center accordion-content-item"}>
@@ -37,20 +64,41 @@ const Colors = () => {
           type="color"
           id="background"
           name="head"
-          defaultValue="#e66465"
+          value={colors.background}
+          onChange={(e) => {
+            handleColorChange("background", e.target.value);
+          }}
         />
         <label htmlFor="background">Background</label>
       </div>
       <div className={"flex flex-col items-center accordion-content-item"}>
-        <input type="color" id="foreground" name="head" defaultValue="#fff" />
+        <input
+          type="color"
+          id="foreground"
+          name="head"
+          value={colors.foreground}
+          onChange={(e) => {
+            handleColorChange("foreground", e.target.value);
+          }}
+        />
         <label htmlFor="foreground">Foreground</label>
       </div>
     </div>
   );
 };
 
-const LabelInput = () => {
-  return <input className={"input"} type={"text"} placeholder={"Label"} />;
+const LabelInput = ({ label, setLabel }) => {
+  return (
+    <input
+      className={"input"}
+      type={"text"}
+      value={label}
+      onChange={(e) => {
+        setLabel(e.target.value);
+      }}
+      placeholder={"Label"}
+    />
+  );
 };
 
 export default SettingsCard;
